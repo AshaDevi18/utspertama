43c0b44c737e5a3d8cc043cc97aba25a3f54b336
AshaDevi18 <ashakara28@gmail.com>
2023-05-11 16:39:20 +0700

    UTS

--- pengguna/Mhs.java
+++ pengguna/Mhs.java
@@ -1,7 +1,18 @@
 package pengguna;
-// HELLO WORD
-class Mhs {
-    Mhs () {
 
+class Mhs {
+    String.nama;
+    String.nim;
+    int.smt;
+
+    void setNama(String arg_nama){
+        this.nama= arg_nama;
     }
-}
+
+    void setNim(String arg_nim){
+        this.nim= arg_nim;
+    }
+
+    void setSmt(String arg_smt){
+        this.smt= arg_smt;
+    }
--- pengguna/User.java
+++ pengguna/User.java
@@ -2,4 +2,16 @@ package pengguna;
 
 public class User {
 
+private String username;
+private String password;
+
+public void setUsername(String username){
+    this.Username = username;
+}
+
+public string getUsername(){
+    return this.username;
+}
+
+public void setPassword(string paasword)
 }
