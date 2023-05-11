e7b541f11e7b332075440f8fdfb8a2bd04aacbca
AshaDevi18 <ashakara28@gmail.com>
2023-05-11 16:45:43 +0700

    UTS

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
-class Main {
-    public static void main(String[] args) {
+class maim {
+    public static void main (String[] args){
+Mhs mhs =new(username: "ini_username , password");
 
+mhs.setNama(nama:"Putri Dila");
+mhs.setNim(nim:"G123");
+mhs.setSmt(smt:4);
+
+System.out.println("Nama Mhs: " + mhs.getNama());
+System.out.println("Nim Mhs: " + mhs.getNim());
+System.out.println("Smt Mhs: " + mhs.getSmt());
+System.out.println("Username: " + mhs.getUsername());
+System.out.println("Password: " + mhs.getPassword());
     }
 }
+
