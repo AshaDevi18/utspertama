b612366d909d5f9f9a98f63cdafd12490639aaf6
AshaDevi18 <132544215+AshaDevi18@users.noreply.github.com>
2023-05-17 17:37:27 +0700

    Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,19 @@
-class maim {
-    public static void main (String[] args){
-Mhs mhs =new(username: "ini_username , password");
+import pengguna.Mhs;
 
-mhs.setNama(nama:"Putri Dila");
-mhs.setNim(nim:"G123");
-mhs.setSmt(smt:4);
+class Main {
+    public static void main(String[] args) {
 
-System.out.println("Nama Mhs: " + mhs.getNama());
-System.out.println("Nim Mhs: " + mhs.getNim());
-System.out.println("Smt Mhs: " + mhs.getSmt());
-System.out.println("Username: " + mhs.getUsername());
-System.out.println("Password: " + mhs.getPassword());
+        Mhs mhs = new Mhs("Ini_Username", "Ini_Password", "Asha Adonia D E", "G.111.19.0018", 8);
+
+        // mhs.setNama("Asha Adonia D E");
+        // mhs.setNim("G.111.19.0018");
+        // mhs.setSmt(8);
+
+        System.out.println("Nama Mhs : " + mhs.getNama());
+        System.out.println("Nim Mhs : " + mhs.getNim());
+        System.out.println("Smt Mhs : " + mhs.getSmt());
+        System.out.println("Username : " + mhs.getUsername());
+        System.out.println("Password : " + mhs.getPassword());
+
     }
 }
-
